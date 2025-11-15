a31b415991b15a452a005444995a7490d857e8a8
David Strawn <strawndk@gmail.com>
2025-11-15 10:37:16 -0600

    update port

--- server.js
+++ server.js
@@ -46,5 +46,5 @@ app.use('/api', captionRoutes)
 app.use('/api', feedbackRoutes)
 app.use('/api', errorRoutes)
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
